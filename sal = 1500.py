35e288d9f8cad6126f10edc00487b138625b3213
Felipe.eloy <felipe.eloy3108@gmail.com>
2026-03-18 22:23:11 -0300

    praticando python

---
+++ sal = 1500.py
@@ -0,0 +1,23 @@
+
+
+
+#exercicio 1
+
+n1 = 10
+n2 = 7
+n3 = 3
+
+print("sua primeira nota em portgues é," , n1)
+print("sua segunda nota em portgeues é," , n2)
+print("sua teceira nota em portugues é," ,n3)
+ 
+r1 = n1 >= 6
+r2 = n2 >= 6
+r3 = n3 >= 6 
+
+print("foi aprovado", r1)
+print("foi aprovado" , r2)
+print("foi aprovado" , r3)
+
+
+
